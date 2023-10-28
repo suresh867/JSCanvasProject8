bf94b84f4dff2913bf069b6f99acedb12525214b
suresh867 <sureshpathak867@gmail.com>
2023-10-28 10:53:42 +0545

    added enemies to game

---
+++ enemies.js
@@ -0,0 +1,27 @@
+class Enemy{
+    constructor(){
+        this.frameX = 0;
+        this.FrameY = 0;
+        this.fps = 20;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
+    }
+    update(){
+
+    }
+    draw(){
+
+    }
+}
+
+class FlyingEnemy extends Enemy{
+
+}
+
+class GroundEnemy extends Enemy{
+
+}
+
+class ClimbingEnemy extends Enemy{
+    
+}
