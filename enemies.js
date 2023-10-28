d896cc1ca33253f78402049ac837c826b23f1760
suresh867 <sureshpathak867@gmail.com>
2023-10-28 12:05:38 +0545

    added another spider enemy type;

--- enemies.js
+++ enemies.js
@@ -67,5 +67,24 @@ export class GroundEnemy extends Enemy{
 }
 
 export class ClimbingEnemy extends Enemy{
-
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 120;
+        this.height = 144;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.image = document.getElementById('enemySpider');
+        this.speedX = 0;
+        this.speedY = Math.random() > 0.5 ? 1: -1;
+        this.maxFrame = 5;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+    }
+    draw(context){
+        super.draw(context);
+        if(this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+        if(this.y < -this.height) this.markedForDeletion = true; 
+    }
 }
--- script.js
+++ script.js
@@ -47,7 +47,8 @@ window.addEventListener('load', function(){
             })
         }
         addEnemy(){
-            if(this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this)); 
+            if(this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+            else if(this.speed > 0) this.enemies.push(new ClimbingEnemy(this)); 
             this.enemies.push(new FlyingEnemy(this));
         }
     }
