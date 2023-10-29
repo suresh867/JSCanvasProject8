762463b8918fb95b37d1ca343dd65251bd8b286d
suresh867 <sureshpathak867@gmail.com>
2023-10-29 21:56:11 +0545

    fire particles added

--- particles.js
+++ particles.js
@@ -27,3 +27,28 @@ export class Dust extends Particle{
         context.fill();
     }
 }
+
+export class Fire extends Particle{
+    constructor(game, x, y){
+        super(game);
+        this.image = document.getElementById('fireImg');
+        this.size = Math.random() * 100 + 50;
+        this.x = x;
+        this.y = y;
+        this.speedX = 1;
+        this.speedY = 1;
+        this.angle = 0;
+        this.va = Math.random() * 0.2 - 0.1;
+    }
+    update(){
+        super.update();
+        this.angle += this.va;
+    }
+    draw(context){
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        context.drawImage(this.image, -this.size * 0.5, -this.size * 0.5, this.size, this.size);
+        context.restore();
+    }
+}
